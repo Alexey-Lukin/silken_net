12dc4794c5dc84ca1671ca751c4aec4f978188a2
Alexey Lukin <realvirtuozzz@gmail.com>
2026-02-27 20:47:12 +0200

    Add organization association and naas_contracts
    Added association to organization and naas_contracts.

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 class Cluster < ApplicationRecord
-  # Якщо кластер видаляється, дерева і шлюзи не знищуються,
-  # а просто "відв'язуються" (nullify), щоб не втратити історичні дані.
+  # Кожен лісовий кластер має належати юридичній особі (інвестору/власнику)
+  belongs_to :organization
+  
   has_many :trees, dependent: :nullify
   has_many :gateways, dependent: :nullify
+  has_many :naas_contracts, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :region, presence: true
 
-  # Додаткові поля, які ми тримаємо в базі:
   # - geojson_polygon: jsonb (Межі лісу для карти)
   # - climate_type: string (Наприклад, "Помірний", "Тропічний")
 
